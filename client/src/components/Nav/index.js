ea947b298801b853b5792713cc12a317a9a381d9
ANS <36783010+answebdev@users.noreply.github.com>
2019-01-19 12:03:54 -0800

    changed to fixed-top navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ import './Nav.css';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary"><img src={owl} alt="Owl" />
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-primary"><img src={owl} alt="Owl" />
       <a className="navbar-brand" href="/">
       &nbsp;&nbsp;Google Books Search App
       </a>
